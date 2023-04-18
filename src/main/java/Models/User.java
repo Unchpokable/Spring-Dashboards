7b2f80115ee5575fe9064f13a33e5d98475103a7
Unchpokable <Unchpokable.official@gmail.com>
2023-04-19 04:33:18 +0500

    added fields to User

--- src/main/java/Models/User.java
+++ src/main/java/Models/User.java
@@ -1,4 +1,26 @@
 package Models;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
 public class User {
+        @Id
+        @GeneratedValue
+        private Long m_id;
+
+        @Column(name = "username")
+        private String m_username;
+
+        @Column(name = "first_name")
+        private String m_firstName;
+
+        @Column(name = "lastName")
+        private String m_lastName;
+
+        @Column(name = "workspaces")
+        @OneToMany(mappedBy = "owner")
+        private List<Workspace> m_workspaces;
 }
