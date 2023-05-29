479321165574a878ad0ae87ddfd02ef4c74def6f
Unchpokable <Unchpokable.official@gmail.com>
2023-05-29 06:02:32 +0500

    Removed REST API methods, some mappings reworked to work with thymeleaf template engine

--- src/main/java/project/dashboard/controllers/DashboardController.java
+++ src/main/java/project/dashboard/controllers/DashboardController.java
@@ -7,8 +7,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import project.dashboard.models.Dashboard;
 import project.dashboard.models.User;
-import project.dashboard.models.Workspace;
 import project.dashboard.services.DashboardService;
+import project.dashboard.services.WorkspaceService;
 
 import java.security.Principal;
 import java.util.List;
@@ -17,53 +17,59 @@ import java.util.List;
 @RequestMapping("/dashboards")
 public class DashboardController {
     private final DashboardService dashboardService;
+    private final WorkspaceService workspaceService;
 
     @Autowired
-    public DashboardController(DashboardService dashboardService) {
+    public DashboardController(DashboardService dashboardService, WorkspaceService workspaceService) {
         this.dashboardService = dashboardService;
+        this.workspaceService = workspaceService;
     }
 
-    @PostMapping("/{workspaceId}/create")
-    public String createDashboard(@PathVariable Long workspaceId, @RequestParam("name") String title, Model model) {
+    @PostMapping("/create/{workspaceId}")
+    public String createDashboard(Principal principal, @PathVariable Long workspaceId, @RequestBody String title,
+                                  Model model) {
         var dashboard = dashboardService.createDashboard(workspaceId, title);
         if (dashboard == null) {
-            model.addAttribute("message", "Something went wrong");
-            return "/{workspaceId}/create";
+            return "boards";
         }
-        return " ";
+
+        var dashboards = workspaceService.getWorkspaceDashboards(workspaceId);
+        model.addAttribute("ownedBoards", dashboards);
+        return "boards";
     }
 
-    @DeleteMapping("/remove/{dashboardId}")
-    public String removeDashboard(@PathVariable Long dashboardId, Model model) {
-        if (dashboardService.removeDashboard(dashboardId))
-            return " ";
-        model.addAttribute("message", "Something went wrong");
-        return "/remove/{dashboardId}";
+    @PostMapping("/remove/{dashboardId}")
+    public String removeDashboard(@PathVariable Long dashboardId, @RequestParam("rootWorkspace") Long wsId, Model model) {
+        if (!dashboardService.removeDashboard(dashboardId))
+            return "boards";
+
+        var boards = workspaceService.getWorkspaceDashboards(wsId);
+        model.addAttribute("ownedBoards", boards);
+        return "boards";
     }
 
-    @PutMapping("/{dashboardId}/rename")
-    public ResponseEntity<?> renameDashboard(@PathVariable Long dashboardId, @RequestParam("name") String name) {
-        if (dashboardService.renameDashboard(dashboardId, name))
-            return ResponseEntity.ok().build();
-        return ResponseEntity.badRequest().build();
-    }
-
-    @PutMapping("/{dashboardId}/addUser")
-    public ResponseEntity<?> addDashboardMember(@PathVariable Long dashboardId, @RequestBody User user) {
-        if (dashboardService.addDashboardMember(dashboardId, user)) {
-            return ResponseEntity.ok().build();
-        }
-        return ResponseEntity.badRequest().build();
-    }
-
-//    @GetMapping("/dashboards")
-//    public String getUserDashboards(Principal principal, Model model) {
-//        String nickname = principal.getName();
-//
-//        List<Dashboard> dashboards = dashboardService.getWorkspaceDashboards(nickname);
-//        model.addAttribute("dashboardsCount", dashboards.size());
-//        model.addAttribute("dashboards", dashboards);
-//        return "dashboards";
-//
+//    @PutMapping("/{dashboardId}/rename")
+//    public ResponseEntity<?> renameDashboard(@PathVariable Long dashboardId, @RequestParam("name") String name) {
+//        if (dashboardService.renameDashboard(dashboardId, name))
+//            return ResponseEntity.ok().build();
+//        return ResponseEntity.badRequest().build();
 //    }
+
+//    @PutMapping("/{dashboardId}/addUser")
+//    public ResponseEntity<?> addDashboardMember(@PathVariable Long dashboardId, @RequestBody User user) {
+//        if (dashboardService.addDashboardMember(dashboardId, user)) {
+//            return ResponseEntity.ok().build();
+//        }
+//        return ResponseEntity.badRequest().build();
+//    }
+
+    @GetMapping("/dashboards")
+    public String getUserDashboards(Principal principal, Model model, @RequestParam("workspaceId") Long workspaceId) {
+        String nickname = principal.getName();
+
+        List<Dashboard> dashboards = dashboardService.getWorkspaceDashboards(nickname);
+        model.addAttribute("dashboardsCount", dashboards.size());
+        model.addAttribute("dashboards", dashboards);
+        return "dashboards";
+    }
 }
