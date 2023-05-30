da3e9b569840423339bfa62ef2642c2fc84a1bed
Unchpokable <Unchpokable.official@gmail.com>
2023-05-30 06:52:02 +0500

    Special data classes to more friendly recieve POST-requests bodies

---
+++ src/main/java/project/dashboard/models/internals/TaskCardCreationRequestData.java
@@ -0,0 +1,62 @@
+package project.dashboard.models.internals;
+
+import project.dashboard.models.TaskCategory;
+
+import java.time.Instant;
+
+public class TaskCardCreationRequestData {
+    private String assignedUser;
+    private Long dashboardId;
+    private String title;
+    private String description;
+    private String deadline;
+    private TaskCategory category;
+
+    public String getAssignedUser() {
+        return assignedUser;
+    }
+
+    public void setAssignedUser(String assignedUser) {
+        this.assignedUser = assignedUser;
+    }
+
+    public Long getDashboardId() {
+        return dashboardId;
+    }
+
+    public void setDashboardId(Long dashboardId) {
+        this.dashboardId = dashboardId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(String deadline) {
+        this.deadline = deadline;
+    }
+
+    public TaskCategory getCategory() {
+        return category;
+    }
+
+    public void setCategory(TaskCategory category) {
+        this.category = category;
+    }
+}
